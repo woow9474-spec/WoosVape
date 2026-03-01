8d429eace09e5c0acd43ddc7d432a9fb11d2c784
woow9474-spec <woow9474@gmail.com>
2026-02-28 19:53:03 -0800

    Update 13246639586.lua

--- games/13246639586.lua
+++ games/13246639586.lua
@@ -15,7 +15,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function() 
-			return game:HttpGet('https://raw.githubusercontent.com/woo9474-spec/WoosVape/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
+			return game:HttpGet('https://raw.githubusercontent.com/woow9474-spec/WoosVape/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true) 
 		end)
 		if not suc or res == '404: Not Found' then 
 			error(res) 
